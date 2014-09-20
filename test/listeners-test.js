35979534b1b22225b0346dacf8f14006b700b4ac
Maximilian Antoni <mail@maxantoni.de>
2014-09-20 12:21:06 +0200

    Pass opts to glob-store

--- test/listeners-test.js
+++ test/listeners-test.js
@@ -14,19 +14,17 @@ var util = require('./fixture/util');
 
 
 describe('listeners', function () {
-  var e;
-
-  beforeEach(function () {
-    e = new Emitter();
-  });
 
   it('returns an empty array by default', function () {
+    var e = new Emitter();
+
     var a = e.listeners();
 
     assert.deepEqual(a, []);
   });
 
   it('passes args to iterator', function () {
+    var e = new Emitter();
     e.iterator = util.stub(e.iterator);
 
     e.listeners('abc', { matchers : false });
@@ -36,12 +34,27 @@ describe('listeners', function () {
   });
 
   it('returns original once listener', function () {
+    var e = new Emitter();
     var f = util.noop();
     e.once('a', f);
 
     var a = e.listeners();
 
     assert.deepEqual(a, [f]);
+  });
+
+  it('returns reverse listeners', function () {
+    var e = new Emitter({ reverse : true });
+    var l1 = util.noop();
+    var l2 = util.noop();
+    var l3 = util.noop();
+    e.addListener('a', l1);
+    e.addListener('a', l2);
+    e.addListener('a', l3);
+
+    var a = e.listeners('a');
+
+    assert.deepEqual(a, [l3, l2, l1]);
   });
 
 });
